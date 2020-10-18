f81c62d1f4e9ebcc51d41aca13aa4e3c5c698493
Keshav Sharma <keshavgalav07@gmail.com>
2020-10-18 13:39:14 +0530

    Superhero app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
 import React, { useState } from 'react';
-import './App.css';
+import Navbar from './Navbar';
+import SearchBar from './SearchBar';
+import SearchResults from './SearchResults';
 
 function App() {
-  const [name ,setName] =useState('Keshav');
-  
-  function changeName (){
-    setName('Mark');
+  const [searchText, setSearchText] = useState('');
+  const [superheroData, setSuperheroData] = useState([]);
+
+  async function searchSuperHeroes () {
+    const response = await fetch(`https://www.superheroapi.com/api.php/10219177700206566/search/${searchText}`);
+    const data = await response.json();
+    console.log("searchSuperHeroes -> data", data)
+
+    setSuperheroData(data.results);
   }
+
+  function handleChange (e) {
+    const searchTerm = e.target.value;
+    
+    setSearchText(searchTerm);
+    if (searchTerm.length === 0) {
+      setSuperheroData([]);
+    }
+    if (searchTerm.length > 3) {
+      searchSuperHeroes();
+    }
+  }
+
   return (
     <div className="App">
-      
-        Helo {name}
-        <br/>
-        <button onClick={changeName}>Change</button>
-          </div>
+      <Navbar />
+      <div className="main">
+        <SearchBar searchText={searchText} handleChange={handleChange} />
+        <SearchResults superheroData={superheroData} />
+      </div>
+    </div>
   );
 }
 
---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Navbar(props) {
+  return (
+        <div className="navbar">
+
+          <span id="logo">SuperHero Hunter</span>
+
+          </div>
+  );
+}
+
+export default Navbar;
