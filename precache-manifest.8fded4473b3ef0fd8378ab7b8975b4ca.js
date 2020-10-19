d114484d991c98a5ec3d3978a6ef6248de89ff24
Keshav Sharma <keshavgalav07@gmail.com>
2020-10-19 16:55:04 +0530

    Updates

---
+++ precache-manifest.8fded4473b3ef0fd8378ab7b8975b4ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4df3aa155114733df0d02b3aebff43f2",
+    "url": "/superheroapp/index.html"
+  },
+  {
+    "revision": "f79d71509d6cbea2ef72",
+    "url": "/superheroapp/static/css/main.bb30bc1f.chunk.css"
+  },
+  {
+    "revision": "fb86e9fcd7bec5bfb627",
+    "url": "/superheroapp/static/js/2.488af992.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/superheroapp/static/js/2.488af992.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f79d71509d6cbea2ef72",
+    "url": "/superheroapp/static/js/main.97882b25.chunk.js"
+  },
+  {
+    "revision": "762df662787cca0e7118",
+    "url": "/superheroapp/static/js/runtime-main.92fdc385.js"
+  }
+]);
